e7e4988ba6f60211355b1586958b121195e62485
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-04-08 18:39:45 +0430

    hashicorp consul

---
+++ ConsulSample/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsulSample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        // GET api/values  
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            //to show the result of different usges   
+            return new string[] { _configuration["FeatureManagement:Beta"] };
+        }
+
+    }
+}
