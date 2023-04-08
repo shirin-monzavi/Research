3d656a757297051db894a746516b1ff6a776150c
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-04-08 18:40:24 +0430

    hashi corp

--- ConsulSample/Program.cs
+++ ConsulSample/Program.cs
@@ -1,3 +1,5 @@
+using Winton.Extensions.Configuration.Consul;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.ConfigureAppConfiguration((context, config) =>
+{
+    var consulHost = config.Build().GetValue<string>("ConsulServer");
+    config.AddConsul("FeatureManagement",
+        options =>
+        {
+            options.ConsulConfigurationOptions =
+                                          cco => { cco.Address = new Uri("http://127.0.0.1:8500"); };
+            options.Optional = true;
+            options.ReloadOnChange = true;
+            options.OnLoadException = exceptionContext => { exceptionContext.Ignore = true; };
+        });
+});
 
 var app = builder.Build();
 
