b36e76db515d625de9ae8265a7b9fb333c18a340
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-04-29 13:15:04 +0430

    MonitorSample

---
+++ MonitorSample/Program.cs
@@ -0,0 +1,38 @@
+﻿class Program
+{
+    static object monitor = new object();
+    public static void Main(string[] arg)
+    {
+        Thread t1 = new Thread(Worker);
+        Thread t2 = new Thread(Boss);
+        t1.Start();
+        Thread.Sleep(1000);
+        t2.Start();
+
+    }
+
+    static void Worker()
+    {
+        lock (monitor)
+        {
+            Console.WriteLine("i am free");
+            Monitor.Wait(monitor);
+            Console.WriteLine("Working");
+            Monitor.Pulse(monitor);
+        }
+    }
+
+    static void Boss()
+    {
+        lock (monitor)
+        {
+            Console.WriteLine("Do it");
+
+            Monitor.Pulse(monitor);
+
+            Monitor.Wait(monitor);
+            Console.WriteLine("well done");
+        }
+    }
+
+}
