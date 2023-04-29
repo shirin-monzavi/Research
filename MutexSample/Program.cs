ed15421822aed0c9333616d3172bf676860f4ccc
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-04-29 16:56:10 +0430

    mutex sample

---
+++ MutexSample/Program.cs
@@ -0,0 +1,25 @@
+﻿class OneAtATimePlease
+{
+    static void Main()
+    {
+        // Naming a Mutex makes it available computer-wide. Use a name that's
+        // unique to your company and application (e.g., include your URL).
+        bool createdNew;
+        using (var mutex = new Mutex(false, "test", out createdNew))
+        {
+            if (createdNew)
+            {
+                RunProgram();
+            }
+            Console.WriteLine("Another app instance is running. Bye!");
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+            return;
+        }
+    }
+
+    static void RunProgram()
+    {
+        Console.WriteLine("Running. Press Enter to exit");
+        Console.ReadLine();
+    }
+}
