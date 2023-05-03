06513461f18e8ca8c5ee31eb5cddb3d2570aab8d
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-05-03 17:39:17 +0430

    ExcludeFromCodeCoverage

--- CodeCoverageSample/Divider.cs
+++ CodeCoverageSample/Divider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace CodeCoverageSample
 {
     public class Divider
     {
+        [ExcludeFromCodeCoverage]
         public int Divide(int x, int y)
         {
             if (y != 0)
