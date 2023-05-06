34095090655a8196b988f01b427478906c24e839
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-05-06 11:37:24 +0430

    taskfrom result

---
+++ TaskFromResultSample/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await Task.FromResult(Calculate(10, 5));
+    }
+
+    public static int Calculate(int x, int y)
+    {
+        return x + y;
+    }
+}
