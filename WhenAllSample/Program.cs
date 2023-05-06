cbeb8c1284e621f71aa6cfc5255bd1444bddbbb8
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-05-06 12:04:22 +0430

    when all

---
+++ WhenAllSample/Program.cs
@@ -0,0 +1,33 @@
+﻿class Prgoram
+{
+    public static async Task Main(string[] arg)
+    {
+        var task1 = Task.Run(() =>
+          {
+              DoWork(1);
+          });
+
+        var task2 = Task.Run(() =>
+         {
+             DoWork(2);
+         });
+
+
+        var task3 = Task.Run(() =>
+        {
+            DoWork(3);
+        });
+
+        await Task.WhenAll(task1, task2, task3);
+
+    }
+
+    static async Task<int> DoWork(int taskId)
+    {
+        Console.WriteLine("Task is started {0}", taskId);
+
+        Console.WriteLine("Task Is Finished {0}", taskId);
+
+        return taskId;
+    }
+}
