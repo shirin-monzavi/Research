1252787d0a02791624a14be8475ec8787d44b08e
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-05-06 16:28:02 +0430

    spec flow

---
+++ BDDSample/Calculator.cs
@@ -0,0 +1,16 @@
+﻿public class Calculator
+{
+    public int FirstNumber { get; set; }
+
+    public int SecondNumber { get; set; }
+
+    public int Add()
+    {
+        return FirstNumber + SecondNumber;
+    }
+
+    public int Subtract()
+    {
+        return FirstNumber - SecondNumber;
+    }
+}
---
+++ SpecFlowCalculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,51 @@
+using FluentAssertions;
+
+namespace SpecFlowCalculator.Specs.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+        private readonly Calculator _calculator = new Calculator();
+        private readonly ScenarioContext _senarioContext;
+        private int _result;
+
+        public CalculatorStepDefinitions(ScenarioContext senarioContext)
+        {
+            _senarioContext = senarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _calculator.FirstNumber = number;
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _result = _calculator.Add();
+        }
+
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            _result = _calculator.Subtract();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _result.Should().Be(result);
+        }
+
+    
+
+    }
+}
