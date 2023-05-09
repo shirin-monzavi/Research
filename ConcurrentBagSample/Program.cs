5664f006d2e54e13221cee5a35474defa7fad102
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-05-09 18:28:29 +0430

    concurrentBag

---
+++ ConcurrentBagSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+
+class Program
+{
+    public static void Main(string [] args)
+    {
+        var concurrentBag = new ConcurrentBag<int>();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            concurrentBag.Add(i);
+        }
+
+        while (concurrentBag.Count>0)
+        {
+            int element;
+            if(concurrentBag.TryTake(out element))
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
