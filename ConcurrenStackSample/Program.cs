2f855ac880852435ca67a90d957d80b9eb362f51
S.monzavi <S.monzavi@DESKTOP-PP7HBIF>
2023-05-09 18:08:13 +0430

    ConcurrentStack

---
+++ ConcurrenStackSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Concurrent;
+
+class Program
+{
+    static void Main(string [] args)
+    {
+        var concurrentStack = new ConcurrentStack<int>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            concurrentStack.Push(i);
+        }
+
+
+        while (concurrentStack.Count>0)
+        {
+            int data;
+            bool success = concurrentStack.TryPop(out data);
+            if (success)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
