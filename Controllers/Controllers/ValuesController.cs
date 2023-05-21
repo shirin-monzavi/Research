8561ecf369a709363c732df9d0b4c411dcf31d4c
shirin monzavi <shirin.monzavi.1372@gmail.com>
2023-05-21 12:43:33 +0330

    addcontrollerformotherassembly

---
+++ Controllers/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Controllers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            return 10;
+        }
+    }
+}
