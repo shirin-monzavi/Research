6f2b84166f38d96d672cc74e5b51b08fb9aabaf8
shirin monzavi <shirin.monzavi.1372@gmail.com>
2023-05-24 14:07:06 +0330

    ADD DECORATOR

---
+++ OrderDecoratorDesignPatternSample/Order.cs
@@ -0,0 +1,58 @@
+﻿namespace OrderDecoratorDesignPatternSample
+{
+    public abstract class OrderBase
+    {
+        public abstract double CalculateTotalOrderPrice();
+    }
+
+    public class RegularOrder : OrderBase
+    {
+        public override double CalculateTotalOrderPrice()
+        {
+            Console.WriteLine("Calculating the total price of a regular order");
+            return 10;
+        }
+    }
+
+    public class Preorder : OrderBase
+    {
+        public override double CalculateTotalOrderPrice()
+        {
+            Console.WriteLine("Calculating the total price of a preorder.");
+            return 9;
+        }
+    }
+
+    public class OrderDecorator : OrderBase
+    {
+        protected OrderBase order;
+
+        public OrderDecorator(OrderBase order)
+        {
+            this.order = order;
+        }
+
+        public override double CalculateTotalOrderPrice()
+        {
+            Console.WriteLine($"Calculating the total price in a decorator class");
+            return order.CalculateTotalOrderPrice();
+        }
+    }
+
+    public class PremiumPreorder : OrderDecorator
+    {
+        public PremiumPreorder(OrderBase order)
+            : base(order)
+        {
+        }
+
+        public override double CalculateTotalOrderPrice()
+        {
+            Console.WriteLine($"Calculating the total price in the {nameof(PremiumPreorder)} class.");
+            var preOrderPrice = base.CalculateTotalOrderPrice();
+
+            Console.WriteLine("Adding additional discount to a preorder price");
+            return preOrderPrice * 0.9;
+        }
+    }
+}
---
+++ OrderDecoratorDesignPatternSample/Program.cs
@@ -0,0 +1,18 @@
+﻿using OrderDecoratorDesignPatternSample;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var regularOrder = new RegularOrder();
+        Console.WriteLine(regularOrder.CalculateTotalOrderPrice());
+        Console.WriteLine();
+
+        var preOrder = new Preorder();
+        Console.WriteLine(preOrder.CalculateTotalOrderPrice());
+        Console.WriteLine();
+
+        var premiumPreorder = new PremiumPreorder(preOrder);
+        Console.WriteLine(premiumPreorder.CalculateTotalOrderPrice());
+    }
+}
