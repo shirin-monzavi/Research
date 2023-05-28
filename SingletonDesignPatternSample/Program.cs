34e7e2b00baca60bbc3ce65144695864fb428393
shirin monzavi <shirin.monzavi.1372@gmail.com>
2023-05-28 16:25:26 +0330

    singletone

---
+++ SingletonDesignPatternSample/Program.cs
@@ -0,0 +1,9 @@
+﻿using SingletonDesignPatternSample;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var instance = Singletone1.CreateInstance();
+    }
+}
---
+++ SingletonDesignPatternSample/Singletone1.cs
@@ -0,0 +1,27 @@
+﻿namespace SingletonDesignPatternSample
+{
+    public class Singletone1
+    {
+        private static Singletone1 singletone1;
+        private static object obj = new object();
+
+        private Singletone1()
+        {
+        }
+
+        public static Singletone1 CreateInstance()
+        {
+            if (singletone1 == null)
+            {
+                lock (obj)
+                {
+                    if (singletone1 == null)
+                    {
+                        return new Singletone1();
+                    }
+                }
+            }
+            return singletone1;
+        }
+    }
+}
