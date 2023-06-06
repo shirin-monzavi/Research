639188d36f20c0c7b9b04fb4498e9b08ee9b21ad
shirin monzavi <shirin.monzavi.1372@gmail.com>
2023-06-06 10:25:11 +0330

    TaskCompletionSource Sample

--- IsAssignableFrom/Program.cs
+++ IsAssignableFrom/Program.cs
@@ -1,8 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using IsAssignableFrom;
 
+var p = new Person();
 var p1 = typeof(Person);
 
 Console.WriteLine(p1.IsAssignableFrom(p.GetType()));
-
-Console.WriteLine(p is Person);
---
+++ TaskCompletionSourceSample/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var t = EvaluateValue("1");
+        Console.WriteLine(t.IsCompleted);
+        Console.WriteLine(t.IsCanceled);
+        Console.WriteLine(t.IsFaulted);
+    }
+
+    public static Task EvaluateValue(string value)
+    {
+        var tcs = new TaskCompletionSource<object>();
+
+        if (value == "1")
+        {
+            tcs.SetResult("1");
+        }
+        else if (value == "2")
+        {
+            tcs.SetCanceled();
+        }
+        else
+        {
+            tcs.SetException(new ArgumentNullException(value));
+        }
+
+        return tcs.Task;
+    }
+}
